47ba10b9dff21b106f1f09090de6f02af1174600
Oscar Ore <oscar.ore2011@gmail.com>
2021-11-30 23:13:02 -0500

    practiced JavaScript conditions

---
+++ app.js
@@ -0,0 +1,15 @@
+// console.log("before conditional")
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Math still works")
+//     console.log(random);
+// }
+// console.log("After conditional")
+
+const dayOfWeek = 'Saturday';
+
+if (dayOfWeek === 'Monday') {
+    console.log('Ugh I hate Mondays!')
+} else if (dayOfWeek === 'Saturday') {
+    console.log("yay! I love saturday")
+}
